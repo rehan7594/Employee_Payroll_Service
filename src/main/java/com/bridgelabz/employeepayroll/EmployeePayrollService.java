8fb7c59d7f1a651a2b211d3c99513b11a8a50d65
rehan7594 <rehansdm94@gmail.com>
2021-06-11 15:45:02 -0700

    Added: Welcome message

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Service");
+	}
+}
